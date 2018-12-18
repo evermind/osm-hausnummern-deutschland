98f66206af4e7af6d3901d6e12cb66e1bddaef04
Michael Wyraz <michael@wyraz.de>
2018-12-18 19:26:18 +0100

    Some output every 3 minutes so that travis does not terminate too early

--- build_tag.sh
+++ build_tag.sh
@@ -7,11 +7,14 @@ source ./env.sh
 URI="${MAP}-latest.osm.pbf"
 FILE="${MAP##*/}-latest.osm.pbf"
 
-wget --quiet -c https://download.geofabrik.de/${URI}
-
 rm -rf docker/data
 mkdir -p docker/data
 (
+  echo "$( date ) Starting download of https://download.geofabrik.de/${URI}"
+  wget --quiet -c https://download.geofabrik.de/${URI}
+
+  echo "$( date ) Download done. Starting processing of multipolygons"
+
   cd docker/data
 
   ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO --config OSM_CONFIG_FILE \
@@ -19,17 +22,26 @@ mkdir -p docker/data
     ../../${FILE} -progress -overwrite -skipfailures -sql \
     "SELECT addr_housenumber addr_house FROM multipolygons WHERE building IS NOT NULL AND addr_housenumber IS NOT NULL"
  
+  echo "$( date ) Multipolygons done. Starting processing of points"
+
   ogr2ogr --config OSM_USE_CUSTOM_INDEXING NO --config OSM_CONFIG_FILE \
     ../../osmconf.ini --config SHAPE_ENCODING UTF-8 -f "ESRI Shapefile" hausnummern \
     ../../${FILE} -progress -overwrite -skipfailures -sql \
     "SELECT addr_housenumber addr_house FROM points WHERE addr_housenumber IS NOT NULL"
+
+  echo "$( date ) All done."
 ) &
 
 PID=$!
 (
+  i=0
   while kill -0 $PID; do
-    echo "Processing ..."
-    sleep 30
+    i=$(( i + 1 ))
+    if [ $i -gt 180 ]; then
+      i=0
+      echo "$( date ) Still building, please wait  ..."
+    fi
+    sleep 1
   done
 ) &
 
